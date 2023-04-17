4fe5bc05c8fac5b6b57c5b65ee2a958033ebed7d
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2023-04-17 19:39:28 +0200

    P1. Added couple of comments

--- charmander.cpp
+++ charmander.cpp
@@ -4,10 +4,13 @@
 Charmander::Charmander(std::string pokeName, std::string weak, std::string strong):
     name(pokeName), weakness(weak), strength(strong){}
 
+
+// prints Charmanders name
 void Charmander::battlecry(){
     std::cout << name << std::endl;
 }
 
+// changes Charmanders name
 void Charmander::setName(std::string name){
     (*this).name = name;
 }
