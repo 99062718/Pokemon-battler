e6e755e3a0ff86331a026a1ce1ce89840e45706b
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2023-05-04 12:48:15 +0200

    P4. Add implementation for Battle constructor

---
+++ arena/battle/battle.cpp
@@ -0,0 +1,8 @@
+#include "battle.hpp"
+
+int Battle::rounds = 0;
+int Battle::battles = 0;
+
+Battle::Battle(std::array<Trainer*, 2> trainers){
+    this->trainers = trainers;
+}
--- arena/battle/battle.hpp
+++ arena/battle/battle.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include "../../trainer/trainer.hpp"
+#include <array>
 
 class Battle{
     static int rounds;
     static int battles;
-    Trainer* trainer[2];
+    std::array<Trainer*, 2> trainers;
 public:
-    Battle(Trainer* trainers[2]);
+    Battle(std::array<Trainer*, 2> trainers);
 };
